c4a0b5e1727bd2181212ade7c533544350846ca6
satyam345 <44867442+satyam345@users.noreply.github.com>
2019-04-27 14:17:51 +0530

    Update and rename untitled5.py to store pixel in csv file.py

--- untitled5.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread('C:/Users/SATYAM/Desktop/28x28/M/image/4D_00002.png')
-grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-npArray = np.array([ elem for singleList in grayscaled for elem in singleList])
-np.savetxt('C:/Users/SATYAM/Desktop/28x28/M/csv/4D_00002.csv', npArray, delimiter=",")
-cv2.waitKey(0)
-cv2.destroyAllWindows()
